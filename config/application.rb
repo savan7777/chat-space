2987b77edd24ed6ff4f43102df364d492931e04b
savan7777 <yokoi0926@gmail.com>
2019-08-02 20:40:52 +0900

    8-2-1

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.action_view.automatically_disable_submit_tag = false
+    config.time_zone = 'Tokyo'
   end
 end
